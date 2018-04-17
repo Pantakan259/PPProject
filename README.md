cb3e49a35aa882a18047da869057acd57a6edd2c
Pantakan259 <pantakan.0088@gmail.com>
2018-04-17 08:00:13 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PPProject
+# PPPJ
+# PPPJ
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import Tab1 from './src/screens/Tab1';
+
+AppRegistry.registerComponent('PPProject', () => Tab1);
